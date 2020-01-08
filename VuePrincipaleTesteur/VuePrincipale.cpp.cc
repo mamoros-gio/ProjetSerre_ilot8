6f4a8bb2b622aee91af8a311af87e3bedb2d716e
ycamilleri <ycamilleri@btslivh.eu>
2020-01-08 16:48:16 +0100

    1er version du code Vue Principal YO

---
+++ VuePrincipaleTesteur/VuePrincipale.cpp.cc
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * File:   VuePrincipale.cpp
+ * Author: snir2g2
+ *
+ * Created on 19 décembre 2019, 14:56
+ */
+
+#include "VuePrincipale.h"
+
+VuePrincipale::VuePrincipale() {
+    widget.setupUi(this);
+}
+
+VuePrincipale::~VuePrincipale() {
+}
+
+VuePrincipale::mettreAJour() {
+    VuePrincipale*VueMeteo= new VuePrincipale;
+    VuePrincipale*VueSerre=new VuePrincipale;
+}
+
+VuePrincipale::addWidget() {
+}
