eba1fad4425d89ecfba45b21ed510e09c5f63bc8
tdoumenc <tdoumenc@btslivh.eu>
2020-01-09 15:37:35 +0100

    Gyro nous casse les *******

--- VueMeteoTesteur/moc_VueMeteo.cpp
+++ VueMeteoTesteur/moc_VueMeteo.cpp
@@ -1,25 +1,45 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'VueMeteo.h'
 **
-** Created by: The Qt Meta Object Compiler version 63 (Qt 4.8.7)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.7.0)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
 #include "VueMeteo.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'VueMeteo.h' doesn't include <QObject>."
-#elif Q_MOC_OUTPUT_REVISION != 63
-#error "This file was generated using the moc from 4.8.7. It"
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.7.0. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
 
 QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_VueMeteo_t {
+    QByteArrayData data[1];
+    char stringdata0[9];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_VueMeteo_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_VueMeteo_t qt_meta_stringdata_VueMeteo = {
+    {
+QT_MOC_LITERAL(0, 0, 8) // "VueMeteo"
+
+    },
+    "VueMeteo"
+};
+#undef QT_MOC_LITERAL
+
 static const uint qt_meta_data_VueMeteo[] = {
 
  // content:
-       6,       // revision
+       7,       // revision
        0,       // classname
        0,    0, // classinfo
        0,    0, // methods
@@ -32,10 +52,6 @@ static const uint qt_meta_data_VueMeteo[] = {
        0        // eod
 };
 
-static const char qt_meta_stringdata_VueMeteo[] = {
-    "VueMeteo\0"
-};
-
 void VueMeteo::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
     Q_UNUSED(_o);
@@ -44,28 +60,21 @@ void VueMeteo::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, vo
     Q_UNUSED(_a);
 }
 
-const QMetaObjectExtraData VueMeteo::staticMetaObjectExtraData = {
-    0,  qt_static_metacall 
-};
-
 const QMetaObject VueMeteo::staticMetaObject = {
-    { &QWidget::staticMetaObject, qt_meta_stringdata_VueMeteo,
-      qt_meta_data_VueMeteo, &staticMetaObjectExtraData }
+    { &QWidget::staticMetaObject, qt_meta_stringdata_VueMeteo.data,
+      qt_meta_data_VueMeteo,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
 };
 
-#ifdef Q_NO_DATA_RELOCATION
-const QMetaObject &VueMeteo::getStaticMetaObject() { return staticMetaObject; }
-#endif //Q_NO_DATA_RELOCATION
 
 const QMetaObject *VueMeteo::metaObject() const
 {
-    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
 }
 
 void *VueMeteo::qt_metacast(const char *_clname)
 {
-    if (!_clname) return 0;
-    if (!strcmp(_clname, qt_meta_stringdata_VueMeteo))
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_VueMeteo.stringdata0))
         return static_cast<void*>(const_cast< VueMeteo*>(this));
     return QWidget::qt_metacast(_clname);
 }
