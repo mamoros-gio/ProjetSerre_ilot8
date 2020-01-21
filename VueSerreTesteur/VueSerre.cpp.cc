cc849f659c389ef732a49f7c3f2bf54cd8cbf714
mamoros <mamoros@btslivh.eu>
2020-01-21 15:59:03 +0100

    résolution de conflit

--- VueSerreTesteur/VueSerre.cpp.cc
+++ VueSerreTesteur/VueSerre.cpp.cc
@@ -19,9 +19,17 @@ VueSerre::VueSerre() {
 VueSerre::~VueSerre() {
     
 }
+
+VueSerre::VueSerre(QString nom1, Infoclimat *uneInfoClimat){
+    widget.setupUi(this);
+    this->nom=nom1;
+    this->infosClimat=uneInfoClimat;
+    this->infosClimat->addObservateur(this);
+}
+
 void VueSerre::mettreAJour(){
-    widget.temperature->setText("22");
-    widget.hygrometrie->setText("45");
-    widget.luminosite->setText("120");
-    widget.positionOuvrants->setText("Ouvert");
+    widget.temperature->setText(QString::number(infosClimat->getTemperature()));
+    widget.hygrometrie->setText(QString::number(infosClimat->getHygrometrie()));
+    widget.luminosite->setText(QString::number(infosClimat->getLuminosite()));
+    widget.positionOuvrants->setText(QString::number(infosClimat->getPositionOuvrants()));
 }
